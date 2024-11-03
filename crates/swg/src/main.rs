119a97c4a85bc74781150bc9ff1675794650bd9f
Omegaice <950526+Omegaice@users.noreply.github.com>
2024-11-03 12:21:08 -0500

    feat(cli): tidied up logging

--- crates/swg/src/main.rs
+++ crates/swg/src/main.rs
@@ -1,7 +1,9 @@
+use std::io::IsTerminal;
+
 use clap::Parser;
-use miette::Result;
-use tracing::Level;
-use tracing_subscriber::FmtSubscriber;
+use miette::{IntoDiagnostic, Result};
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -14,16 +16,24 @@ struct Cli {
 fn main() -> Result<()> {
     better_panic::install();
 
-    // a builder for `FmtSubscriber`.
-    let subscriber = FmtSubscriber::builder()
-        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
-        // will be written to stdout.
-        .with_max_level(Level::INFO)
-        // completes the builder.
-        .finish();
-
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-
     let cli = Cli::parse();
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_ansi(std::io::stdout().is_terminal())
+                .with_file(true)
+                .with_line_number(true)
+                .with_target(false)
+                .without_time()
+                .compact(),
+        )
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy()
+        )
+        .try_init().into_diagnostic()?;
+
     cli.command.handle()
 }
